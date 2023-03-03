6b5c6f2f7de33104b4688d54fdcf49b0f3ef7bf2
David Tolnay <dtolnay@gmail.com>
2023-03-03 15:12:21 -0800

    Release 1.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
 //! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
 
-#![doc(html_root_url = "https://docs.rs/bufsize/1.0.4")]
+#![doc(html_root_url = "https://docs.rs/bufsize/1.0.5")]
 #![allow(
     clippy::let_underscore_untyped,
     clippy::must_use_candidate,
